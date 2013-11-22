52f5ac65cf05704ee63eade78f629e54b51f3aea
Jongmin Kim <atomaths@gmail.com>
2013-11-22 14:50:33 +0900

    test pre git commit

--- init.go
+++ init.go
@@ -1,8 +1,8 @@
 // Copyright 2013 Jongmin Kim. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package helloworld
+package geoip
 
 import (
 	"fmt"
